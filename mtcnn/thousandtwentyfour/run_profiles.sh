6bbe24f81a49657dcef4559d9431d59c4ff6c1fb
yngtodd <young.todd.mk@gmail.com>
2018-04-25 23:04:11 -0400

    More number crunching

--- mtcnn/thousandtwentyfour/run_profiles.sh
+++ mtcnn/thousandtwentyfour/run_profiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in 8 9 10 11 12 13 14 15 16 17 18 19 20 
-do
-  nvprof --metrics flop_count_sp,flop_count_hp --log-file metric$i python main.py --num_result $i
-done
+for i in {21..100} 
+  do
+    nvprof --metrics flop_count_sp,flop_count_hp --log-file metric$i python main.py --num_result $i
+  done
--- mtcnn/thousandtwentyfour/spcount.sh
+++ mtcnn/thousandtwentyfour/spcount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {0..20} 
+for i in {0..100} 
   do
-    grep flop_count_sp ./profiles/metric$i | awk '{s+=$9} END {printf "%.0f,", s}' >> allflops.csv
+    grep flop_count_sp ./profiles/metric$i | awk '{s+=$9} END {printf "%.0f,", s}' >> hundredflops.csv
   done
